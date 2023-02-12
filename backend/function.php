e7fe2f976a38a43fef828e1bb1ac9f3a50a8df5a
deniskorbakov <korbakovd@gmail.com>
2023-02-12 19:07:34 +0300

    обработка ошибки если id указан неверно

--- backend/function.php
+++ backend/function.php
@@ -14,6 +14,17 @@ function getPosts ($connection) {
 
 function getPost($connection, $id) {
     $post = mysqli_query($connection,"SELECT * FROM `posts` WHERE `id` = '$id'");
-    $post = mysqli_fetch_assoc($post);
-    echo json_encode($post);
+    
+    if (mysqli_num_rows($post) === 0) {
+        http_response_code(404);
+        
+        $res = [
+            "status" => false,
+            "message" => "Пост не найден"
+        ];
+        echo json_encode($res);
+    }else {
+        $post = mysqli_fetch_assoc($post);
+        echo json_encode($post);
+    }
 }
